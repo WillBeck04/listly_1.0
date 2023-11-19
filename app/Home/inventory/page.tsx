30975c19c7bd79f543ed1629759d6c2db68b0e23
WillBeck04 <118110165+WillBeck04@users.noreply.github.com>
2023-11-18 22:57:20 -0500

    ojlhn

--- app/Home/inventory/page.tsx
+++ app/Home/inventory/page.tsx
@@ -1,139 +1,100 @@
-"use client";
-import Link from 'next/link';
+"use client"
+// pages/items.tsx
 import React, { useEffect, useState } from 'react';
 
-interface InventoryItem {
+interface Item {
   id: number;
   name: string;
-  quantity: number;
 }
 
-const UpdateInventory: React.FC = () => {
-  const [inventoryItems, setInventoryItems] = useState<InventoryItem[]>([]);
+const ItemsPage: React.FC = () => {
+  const [items, setItems] = useState<Item[]>([]);
   const [newItemName, setNewItemName] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
-    fetchInventory();
+    fetchItems();
   }, []);
 
-  const fetchInventory = async () => {
+  const fetchItems = async () => {
     try {
-      const token = localStorage.getItem('token');
-      const response = await fetch('http://localhost:8080/api/inventory', {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        setInventoryItems(data.inventory);
-      } else {
-        setErrorMessage('Failed to fetch inventory.');
-      }
+      const response = await fetch('http://localhost:8080/items');
+      const data: Item[] = await response.json();
+      setItems(data);
     } catch (error) {
-      console.error('Fetch Inventory Error:', error);
-      setErrorMessage('An error occurred while fetching inventory.');
+      console.error('Error fetching items:', error);
     }
   };
 
-  const updateInventoryItem = async (item: InventoryItem, newQuantity: number) => {
+  const handleAddItem = async () => {
     try {
-      const token = localStorage.getItem('token');
-      await fetch(`http://localhost:8080/api/inventory/${item.id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify({ quantity: newQuantity })
-      });
-
-      fetchInventory(); // Refresh inventory after update
-    } catch (error) {
-      console.error('Update Inventory Error:', error);
-      setErrorMessage('An error occurred while updating inventory.');
-    }
-  };
-
-  const handleAdd = (item: InventoryItem) => {
-    updateInventoryItem(item, item.quantity + 1);
-  };
-
-  const handleRemove = (item: InventoryItem) => {
-    if (item.quantity > 0) {
-      updateInventoryItem(item, item.quantity - 1);
-    }
-  };
-
-  const handleAddNewItem = async () => {
-    if (!newItemName) {
-      setErrorMessage('Please enter a name for the new item.');
-      return;
-    }
-
-    try {
-      const token = localStorage.getItem('token');
-      await fetch('http://localhost:8080/api/inventory', {
+      await fetch('http://localhost:8080/items', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify({ name: newItemName })
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: newItemName }),
       });
-
       setNewItemName('');
-      fetchInventory(); // Refresh inventory after adding new item
+      fetchItems();
     } catch (error) {
-      console.error('Add New Inventory Item Error:', error);
-      setErrorMessage('An error occurred while adding a new inventory item.');
+      console.error('Error adding item:', error);
     }
   };
-    return (
-        <div className="p-4 bg-gray-100 min-h-screen">
-            <h2 className="text-3xl font-bold text-center mb-6 text-cyan-500">Update Inventory</h2>
-            {errorMessage && <p className="text-red-500 text-center">{errorMessage}</p>}
-            <div className="mb-4">
-                <input
-                    type="text"
-                    placeholder="New Item Name"
-                    value={newItemName}
-                    onChange={(e) => setNewItemName(e.target.value)}
-                    className="px-3 py-2 border border-gray-300 rounded"
-                />
-                <button
-                    onClick={handleAddNewItem}
-                    className="ml-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-                >
-                    Add New Item
-                </button>
-            </div>
-            <div className="flex flex-wrap justify-center gap-4">
-                {inventoryItems.map((item) => (
-                    <div key={item.id} className="bg-white p-4 rounded-lg shadow-md w-64">
-                        <div className="flex justify-between items-center mb-2">
-                            <span className="text-lg font-medium text-gray-800">{item.name}</span>
-                            <span className="font-semibold text-gray-700">
-                                {item.quantity > 0 ? `Qty: ${item.quantity}` : "Sold Out" }
-                            </span>
-                        </div>
-                        <div className="flex justify-center">
-                            <button onClick={() => handleRemove(item)} className="px-4 py-2 bg-red-500 text-white rounded-l-lg hover:bg-red-600 transition-colors duration-300 disabled:opacity-50" disabled={item.quantity === 0}>-</button>
-                            <button onClick={() => handleAdd(item)} className="px-4 py-2 bg-green-500 text-white rounded-r-lg hover:bg-green-600 transition-colors duration-300">+</button>
-                        </div>
-                    </div>
-                ))}
-            </div>
-            <Link href="/Home/restock_suggestion">
-                <button className="mt-6 w-full flex justify-center items-center px-6 py-3 bg-cyan-500 text-white font-bold rounded-full hover:bg-cyan-600 transition-colors duration-300 ease-in-out">
-                    See Restock Suggestion
-                </button>
-            </Link>
-        </div>
-    );
-}
 
-export default UpdateInventory;
+  const handleDeleteItem = async (itemId: number) => {
+    try {
+      await fetch(`http://localhost:8080/items/${itemId}`, {
+        method: 'DELETE',
+      });
+      fetchItems();
+    } catch (error) {
+      console.error('Error deleting item:', error);
+    }
+  };
+  const updateItemQuantity = async (itemId: number, newQuantity: number) => {
+    try {
+      await fetch(`http://localhost:8080/items/${itemId}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ quantity: newQuantity }),
+      });
+      fetchItems();
+    } catch (error) {
+      console.error('Error updating item quantity:', error);
+    }
+  };
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-semibold text-center mb-6">Item List</h1>
+      <div className="flex justify-center mb-4">
+        <input
+          type="text"
+          value={newItemName}
+          onChange={(e) => setNewItemName(e.target.value)}
+          className="border border-gray-300 p-2 rounded-l-lg"
+          placeholder="Enter item name"
+        />
+        <button
+          onClick={handleAddItem}
+          className="bg-cyan-100 hover:bg-cyan-200 text-black font-bold py-2 px-4 rounded-r-lg"
+        >
+          Add Item
+        </button>
+      </div>
+      <ul className="space-y-2">
+        {items.map((item) => (
+          <li key={item.id} className="flex justify-between items-center bg-white p-3 rounded shadow">
+            <span className="text-lg">{item.name}</span>
+            <button
+              onClick={() => handleDeleteItem(item.id)}
+              className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-3 rounded"
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ItemsPage;
