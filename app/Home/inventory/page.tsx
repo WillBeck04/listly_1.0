bf7c99c0e7f43d6daa5161c6196606b4efd0608f
salomonlavy10 <salomonlavy@gmail.com>
2023-11-19 00:25:26 -0500

    llll

--- app/Home/inventory/page.tsx
+++ app/Home/inventory/page.tsx
@@ -1,96 +1,127 @@
-"use client"
-// pages/items.tsx
+'use client'
 import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
 
-interface Item {
+interface InventoryItem {
   id: number;
   name: string;
   quantity: number;
 }
 
-const ItemsPage: React.FC = () => {
-  const [items, setItems] = useState<Item[]>([]);
+const UpdateInventory: React.FC = () => {
+  const [inventoryItems, setInventoryItems] = useState<InventoryItem[]>([]);
   const [newItemName, setNewItemName] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
-    fetchItems();
+    fetchInventory();
   }, []);
 
-  const fetchItems = async () => {
-    try {
-      const response = await fetch('http://localhost:8080/items');
-      const data: Item[] = await response.json();
-      setItems(data);
-    } catch (error) {
-      console.error('Error fetching items:', error);
+  const fetchInventory = async () => {
+    const token = localStorage.getItem('token');
+    const response = await fetch('http://localhost:8080/api/inventory', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      setInventoryItems(data);
+    } else {
+      setErrorMessage('Failed to fetch inventory.');
     }
   };
 
-  const handleAddItem = async () => {
-    try {
-      await fetch('http://localhost:8080/items', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name: newItemName }),
-      });
-      setNewItemName('');
-      fetchItems();
-    } catch (error) {
-      console.error('Error adding item:', error);
+  const handleAddNewItem = async () => {
+    if (!newItemName) {
+      setErrorMessage('Please enter a name for the new item.');
+      return;
     }
+
+    const token = localStorage.getItem('token');
+    await fetch('http://localhost:8080/api/inventory', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify({ name: newItemName, quantity: 1 })
+    });
+
+    setNewItemName('');
+    fetchInventory();
+  };
+
+  const handleUpdateItem = async (item: InventoryItem, newQuantity: number) => {
+    const token = localStorage.getItem('token');
+    await fetch(`http://localhost:8080/api/inventory/${item.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify({ quantity: newQuantity })
+    });
+
+    fetchInventory();
   };
 
   const handleDeleteItem = async (itemId: number) => {
-    try {
-      await fetch(`http://localhost:8080/items/${itemId}`, {
-        method: 'DELETE',
-      });
-      fetchItems();
-    } catch (error) {
-      console.error('Error deleting item:', error);
-    }
-  };
-  const updateItemQuantity = async (itemId: number, newQuantity: number) => {
-    try {
-      await fetch(`http://localhost:8080/items/${itemId}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ quantity: newQuantity }),
-      });
-      fetchItems();
-    } catch (error) {
-      console.error('Error updating item quantity:', error);
-    }
+    const token = localStorage.getItem('token');
+    await fetch(`http://localhost:8080/api/inventory/${itemId}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    fetchInventory();
   };
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-semibold text-center mb-6">Item List</h1>
-      <div className="flex justify-center mb-4">
+      <h1 className="text-3xl font-semibold text-center mb-6">Manage Your Inventory</h1>
+      <div className="mb-4">
         <input
           type="text"
+          placeholder="New Item Name"
           value={newItemName}
           onChange={(e) => setNewItemName(e.target.value)}
-          className="border border-gray-300 p-2 rounded-l-lg"
-          placeholder="Enter item name"
+          className="border border-gray-300 p-2 rounded"
         />
         <button
-          onClick={handleAddItem}
-          className="bg-cyan-100 hover:bg-cyan-200 text-black font-bold py-2 px-4 rounded-r-lg"
+          onClick={handleAddNewItem}
+          className="ml-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
         >
           Add Item
         </button>
       </div>
-      <ul className="space-y-2">
-        {items.map((item) => (
-          <li key={item.id} className="flex justify-between items-center bg-white p-3 rounded shadow">
-            <span className="text-lg">{item.name}</span>
-            <button
-              onClick={() => handleDeleteItem(item.id)}
-              className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-3 rounded"
-            >
-              Delete
-            </button>
+      {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+      <ul>
+        {inventoryItems.map(item => (
+          <li key={item.id} className="flex justify-between items-center p-3 bg-white rounded shadow mb-2">
+            <span>{item.name} (Qty: {item.quantity})</span>
+            <div>
+              <button
+                onClick={() => handleUpdateItem(item, item.quantity + 1)}
+                className="bg-green-500 hover:bg-green-600 text-white font-bold py-1 px-3 rounded-l"
+              >
+                +
+              </button>
+              <button
+                onClick={() => handleUpdateItem(item, item.quantity > 0 ? item.quantity - 1 : 0)}
+                className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-3 rounded-r"
+              >
+                -
+              </button>
+              <button
+                onClick={() => handleDeleteItem(item.id)}
+                className="ml-2 bg-gray-500 hover:bg-gray-600 text-white font-bold py-1 px-3 rounded"
+              >
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
@@ -98,4 +129,4 @@ const ItemsPage: React.FC = () => {
   );
 };
 
-export default ItemsPage;
+export default UpdateInventory;
