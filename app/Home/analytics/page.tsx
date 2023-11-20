8ef573922f3a59840684190402dc3c465928dc3a
salomonlavy10 <salomonlavy@gmail.com>
2023-11-19 22:40:53 -0500

    k

--- app/Home/analytics/page.tsx
+++ app/Home/analytics/page.tsx
@@ -1,59 +1,67 @@
-"use client"
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
-import 'chart.js/auto';
+'use client'
+import React, { useEffect, useState } from 'react';
 
-// Static data for demonstration
-const bestSellersData = [12, 19, 3, 5, 2, 3]; // Best Sellers sales data
-const worstSellersData = [2, 3, 20, 5, 1, 4]; // Worst Sellers sales data
+interface InventoryItem {
+  id: number;
+  name: string;
+  quantity: number;
+  boughtQuantity: number;
+  soldQuantity: number;
+}
 
-// Generate restock priority data (example logic)
-const restockPriorityData = bestSellersData.map(sales => 20 - sales); // Just an example calculation
+const AnalyticsPage: React.FC = () => {
+  const [inventoryData, setInventoryData] = useState<InventoryItem[]>([]);
 
-const data = {
-  labels: ['Tomatoes', 'Salad', 'Apples', 'Bananas', 'Oranges', 'Lettuce'],
-  datasets: [
-    {
-      label: 'Best Sellers',
-      data: bestSellersData,
-      backgroundColor: 'rgba(75, 192, 192, 0.2)',
-      borderColor: 'rgba(75, 192, 192, 1)',
-      borderWidth: 1,
-    },
-    {
-      label: 'Worst Sellers',
-      data: worstSellersData,
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
-      borderColor: 'rgba(255, 99, 132, 1)',
-      borderWidth: 1,
-    },
-    {
-      label: 'Restock Priority',
-      data: restockPriorityData,
-      backgroundColor: 'rgba(153, 102, 255, 0.2)',
-      borderColor: 'rgba(153, 102, 255, 1)',
-      borderWidth: 1,
-    },
-  ],
-};
+  useEffect(() => {
+    const fetchInventorySummary = async () => {
+      const token = localStorage.getItem('token');
+      const response = await fetch('http://localhost:8080/api/inventory/summary', {
+        headers: { Authorization: `Bearer ${token}` },
+      });
 
-const options = {
-  scales: {
-    y: {
-      beginAtZero: true,
-    },
-  },
-};
+      if (response.ok) {
+        const data: InventoryItem[] = await response.json();
+        setInventoryData(data);
+      } else {
+        console.error('Failed to fetch inventory summary');
+      }
+    };
 
-function Analytics() {
+    fetchInventorySummary();
+  }, []);
+
+  const calculateLiquidation = (boughtQuantity: number, soldQuantity: number, totalQuantity: number) => {
+    return soldQuantity > 0 ? ((totalQuantity * 100) / soldQuantity).toFixed(2) : 'N/A';
+  };
+
   return (
-    <div className="p-4 bg-gray-100 min-h-screen">
-      <h2 className="text-3xl font-bold text-center mb-6 text-gray-800">Analytics</h2>
-      <div className="max-w-2xl mx-auto">
-        <Bar data={data} options={options} />
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-semibold text-center mb-6">Inventory Analytics</h1>
+      <div className="overflow-x-auto">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            <tr>
+              <th>Item</th>
+              <th>Quantity Bought</th>
+              <th>Quantity Sold</th>
+              <th>Liquidation</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {inventoryData.map((item) => (
+              <tr key={item.id}>
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{item.name}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{item.boughtQuantity}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{item.soldQuantity}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{calculateLiquidation(item.boughtQuantity, item.soldQuantity, item.quantity)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
-}
+};
 
-export default Analytics;
+export default AnalyticsPage;
+
